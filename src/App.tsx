b00fe15f559791e309e026b00cb17501a12e71ee
whddbs627 <whddbs627@gmail.com>
2024-02-06 17:15:40 +0900

    feat: authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,40 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/create-account",
-    element: <CreateAccount />,
-  },
+    {
+        path: "/",
+        element: (
+            <ProtectedRoute>
+                <Layout />
+            </ProtectedRoute>
+        ),
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "profile",
+                element: <Profile />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/create-account",
+        element: <CreateAccount />,
+    },
 ]);
 
 const GlobalStyles = createGlobalStyle`  
@@ -44,13 +52,28 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
+
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </>
-  );
+    const [isLoading, setLoading] = useState(true);
+    const init = async () => {
+        // wait for firebase
+        await auth.authStateReady();
+        setLoading(false);
+    };
+    useEffect(() => {
+        init();
+    }, []);
+    return (
+        <Wrapper>
+            <GlobalStyles />
+            {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+        </Wrapper>
+    );
 }
 
 export default App;
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,15 @@
-export default function Home(){
-    return <h1>Home!</h1>;
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+
+export default function Home() {
+    const navigate = useNavigate();
+    const logOut = () => {
+        auth.signOut();
+        navigate("/login")
+    };
+    return (
+        <h1>
+            <button onClick={logOut}>Log Out</button>
+        </h1>
+    );
 }
